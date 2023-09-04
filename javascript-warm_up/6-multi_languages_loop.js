2acd4f48b184870433977419555439ec6241842f
Anne-15 <wariiyuanne@gmail.com>
2023-09-04 21:29:02 +0300

    looping

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+let firstLine = "C is fun"
+let secondLine = "Python is cool"
+let thirdLine = "JavaScript is amazing"
+
+let i = 3;
+
+for(i=1; i<3; i++){
+    console.log(firstLine)
+    console.log(secondLine)
+    console.log(thirdLine)
+}
