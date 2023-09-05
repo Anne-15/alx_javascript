e86392f649d37e1d8829055de0514fff68a23917
Anne-15 <wariiyuanne@gmail.com>
2023-09-05 11:42:19 +0300

    changes

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,17 +2,11 @@
 const myObject = {
     type: 'object',
     value: 12,
-    incr: incr
   };
 console.log(myObject);
 
 function incr(){
     this.value++;
-}
-
-for (let i = 0; i < 4; i++) {
-    console.log(`{ type: '${myObject.type}', value: ${myObject.value}${i === 0 ? '' : `, incr: [Function]`}}`);
-    myObject.incr();
 }
 
 myObject.incr();
