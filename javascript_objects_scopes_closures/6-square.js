9368d4d0e51623d78e4e4947426f02e47474f7c7
Anne-15 <wariiyuanne@gmail.com>
2023-09-06 17:43:52 +0300

    printing square

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+const Square1 = require('./5-square')
+
+module.exports = class Square extends Square1{
+    constructor(size){
+        super(size, size);
+    }
+
+    charPrint(c){
+        if(c == undefined){
+            c = 'X'
+        }
+        for (let i = 0; i < this.size; i++){
+            let row = '';
+            for (let j = 0; j < this.size; j++){
+                row += c;
+            }
+            console.log(row)
+        }
+    }
+}
