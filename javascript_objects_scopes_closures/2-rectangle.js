d0c3bbc11e5362f317c279c78ac6be1123a5c7c2
Anne-15 <wariiyuanne@gmail.com>
2023-09-06 17:11:14 +0300

    changes

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 module.exports = class Rectangle {
     constructor(w, h) {
         if(w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)){
-            return { };
+            return 'Rectangle {}';
         }
         this.width = w;
         this.height = h;
