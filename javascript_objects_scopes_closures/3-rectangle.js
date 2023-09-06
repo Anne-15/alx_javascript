0ed7539ab245b2c73dc97ad267f4bbdec062eff0
Anne-15 <wariiyuanne@gmail.com>
2023-09-06 17:19:25 +0300

    printing a square

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if(w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)){
+            return 'Rectangle {}';
+        }
+        this.width = w;
+        this.height = h;
+    }
+
+    print(){
+        for (let i = 0; i < this.width; i++){
+            let row = '';
+            for (let j = 0; j < this.height; j++){
+                row += 'X';
+            }
+            console.log(row)
+        }
+    }
+}
