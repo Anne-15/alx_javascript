c3d1596a9d573907902a3615d62678946c40c1ed
Anne-15 <wariiyuanne@gmail.com>
2023-09-06 17:25:05 +0300

    double

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if(w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)){
+            return 'Rectangle {}';
+        }
+        this.width = w;
+        this.height = h;
+    }
+
+    print(){
+        for (let i = 0; i < this.height; i++){
+            let row = '';
+            for (let j = 0; j < this.width; j++){
+                row += 'X';
+            }
+            console.log(row)
+        }
+    }
+
+    double(){
+        for (let i = 0; i < this.height; i++){
+            let row = '';
+            for (let j = 0; j < this.width; j++){
+                row += 'X'*2;
+            }
+            console.log(row)
+        }
+    }
+}
