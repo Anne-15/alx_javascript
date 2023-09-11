3bc970f498cfdb9163575e1e4a23056cb9dcc230
Anne-15 <wariiyuanne@gmail.com>
2023-09-11 20:51:26 +0300

    filtering data

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const url = process.argv[2];
+const characterId = 18;
+
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error(`An error occurred: ${error.message}`);
+    } else if (response.statusCode != 200) {
+        console.error(`An error occurred: ${error.message}`);
+    } else {
+        try {
+            const filmsData = JSON.parse(body).results;
+            const films = filmsData.filter((film) =>
+                film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+            );
+            const numberOfFilms = films.length;
+            console.log(numberOfFilms);
+        } catch (parseError) {
+            console.error(`Error parsing JSON response: ${parseError.message}`);
+        }
+    }
+})
