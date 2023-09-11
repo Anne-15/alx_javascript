e6057037109c22ffc32fa9b6797aa2f2e5345cec
Anne-15 <wariiyuanne@gmail.com>
2023-09-11 20:22:21 +0300

    getting help

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,7 +1,16 @@
 const request = require('request');
 const url = process.argv[2]
-request.get(url)
-    .then((response) => {
-        const status_code = response.status;
+request.get(url, (error, response) => {
+    if (error) {
+        console.error(`An error occurred: ${error.message}`);
+    } else {
+        const status_code = response.statusCode;
         console.log(`code: ${status_code}`);
-    })
+    }
+});
+
+
+
+
+
+
