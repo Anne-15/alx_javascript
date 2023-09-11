8a95a9da2bb0e9bd6d6dbda4f86a6c3228bdb815
Anne-15 <wariiyuanne@gmail.com>
2023-09-11 20:33:15 +0300

    get requests for title

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const id = process.argv[2];
+url = `https://swapi-api.alx-tools.com/api/films/${id}`
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error(`An error occurred: ${error.message}`);
+    } else if (response.statusCode != 200) {
+        console.error(`An error occurred: ${error.message}`);
+    } else {
+        try {
+            const movieData = JSON.parse(body);
+            const movieTitle = movieData.title;
+            console.log(`Title: ${movieTitle}`);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+})
