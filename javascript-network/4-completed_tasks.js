53f183fcab25f2eaf46ddcd7f377c925e10ecea3
Anne-15 <wariiyuanne@gmail.com>
2023-09-12 16:42:53 +0300

    completed tasks

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,6 +1,20 @@
 const request = require('request');
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-request.get(url, (response, body) => {
-    
+request.get(url, (error, response, body) => {
+    //check for errors
+    if (error){
+        console.error(`An error occurred: ${error.message}`);
+    } 
+    //check for success
+    if (response.statusCode == 200) {
+        const data = JSON.parse(body);
+        const count = {};
+        data.forEach((task) => {
+            if (task.completed) {
+              const userId = task.userId.toString();
+              count[userId] = (count[userId] || 0) + 1;
+            }
+          });
+    }
 })
