706c155e39091799a181821ddc3cdc0be360322b
Anne-15 <wariiyuanne@gmail.com>
2023-09-12 16:19:22 +0300

    changes

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -2,7 +2,7 @@ const request = require('request');
 const url = process.argv[2];
 const characterId = 18;
 
-get(url, (error, response, body) => {
+request.get(url, (error, response, body) => {
     if (error) {
         console.error(`An error occurred: ${error.message}`);
     } else if (response.statusCode != 200) {
