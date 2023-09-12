9eab1676552e3257ba29f6984e96835170442760
Anne-15 <wariiyuanne@gmail.com>
2023-09-12 17:17:52 +0300

    changes

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,16 +1,34 @@
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request.get(url, (response, body, error) => {
-    //check for errors
-    if (error){
-        console.error(`An error occurred: ${error.message}`);
-    }
-    //check for success
-    const data = JSON.parse(body).results;
-    data.forEach((person) => {
-        console.log(person.name)
-    })
-    
-})
+// Define the Movie ID as the first command-line argument
+const movieId = process.argv[2];
+
+// Define the Star Wars API URL
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Send a GET request to the API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('HTTP Status Code:', response.statusCode);
+  } else {
+    // Parse the response body as JSON
+    const movieData = JSON.parse(body);
+
+    // Print the characters from the movie
+    // console.log(`Characters from "${movieData.title}":`);
+    movieData.characters.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error('Error:', charError);
+        } else if (charResponse.statusCode !== 200) {
+          console.error('HTTP Status Code:', charResponse.statusCode);
+        } else {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      });
+    });
+  }
+});
