caae08a2a0cbaac0b509be6a4f8a06115b474258
Anne-15 <wariiyuanne@gmail.com>
2023-09-12 16:53:42 +0300

    woow

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,39 +1,28 @@
-// Import the request module
 const request = require('request');
-
-// Define the URL for the JSON data
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-// Make a GET request to the URL
 request.get(url, (error, response, body) => {
-  // Check for errors
-  if (error) {
-    console.error(error);
-    return;
-  }
-  // Parse the JSON data
-  const data = JSON.parse(body);
-
-  // Create an empty object to store the results
-  const results = {};
-
-  // Loop through the data array
-  for (let item of data) {
-    // Get the user id and the completed status
-    let userId = item.userId;
-    let completed = item.completed;
-
-    // If the task is completed, increment the count for the user id
-    if (completed) {
-      // If the user id is not in the results object, initialize it with zero
-      if (!results[userId]) {
-        results[userId] = 0;
-      }
-      // Increment the count by one
-      results[userId]++;
+    //check for errors
+    if (error){
+        console.error(`An error occurred: ${error.message}`);
+    } 
+    //check for success
+    if (response.statusCode == 200) {
+        const data = JSON.parse(body);
+        const count = {};
+        for (let item in data){
+            let userId = item.userId;
+            let completed = item.completed;
+            //if item is completed increase the count of the user id
+            if (completed) {
+                // If the user id is not in the results object, initialize it with zero
+                if(!count[userId]){
+                    count[userId] = 0;
+                }
+                //increment count by 1
+                count[userId]++;
+            }
+        }
+        console.log(count)
     }
-  }
-
-  // Print the results object
-  console.log(results);
-});
+})
