da849834e76ef6fb734c74fa4fdc541ee7e7d057
Anne-15 <wariiyuanne@gmail.com>
2023-09-14 23:58:54 +0300

    changes

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,20 +1,23 @@
 const request = require('request');
 const url = process.argv[2];
-const characterId = 18;
+const characterId = "18";
+let count = 0
 
 request.get(url, (error, response, body) => {
     if (error) {
-        console.error(`An error occurred: ${error.message}`);
+        console.error(`An error occurred1: ${error}`);
     } else if (response.statusCode != 200) {
-        console.error(`An error occurred: ${error.message}`);
+        console.error(`An error occurred2: ${response.statusCode}`);
     } else {
         try {
             const filmsData = JSON.parse(body).results;
             const films = filmsData.filter((film) =>
-                film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-            );
-            const numberOfFilms = films.length;
-            console.log(numberOfFilms);
+            film.characters.forEach(character => {
+                if (character.includes(characterId)) {
+                    count++
+                }
+            }))
+            console.log(count);
         } catch (parseError) {
             console.error(`Error parsing JSON response: ${parseError.message}`);
         }
--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,23 +1,14 @@
 const request = require('request');
-
-// Define the Movie ID as the first command-line argument
 const movieId = process.argv[2];
-
-// Define the Star Wars API URL
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-// Send a GET request to the API
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
   } else if (response.statusCode !== 200) {
     console.error('HTTP Status Code:', response.statusCode);
   } else {
-    // Parse the response body as JSON
     const movieData = JSON.parse(body);
-
-    // Print the characters from the movie
-    // console.log(`Characters from "${movieData.title}":`);
     movieData.characters.forEach((characterUrl) => {
       request(characterUrl, (charError, charResponse, charBody) => {
         if (charError) {
