7bed7a4427239f267ee4ae39d50e5d42016a8d0e
Anne-15 <wariiyuanne@gmail.com>
2023-09-27 12:39:52 +0300

    form submission

---
+++ html_js_validation/3-form-submission.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function(){
+    function handleFormSubmit (event){
+        event.preventDefault();
+        //getting the input values
+        const name = document.getElementById('name');
+        const email = document.getElementById('email');
+        const message = document.getElementById('message');
+    
+         // Validate form fields
+         if (name == '' || email == '') {
+            // Display error message if any required field is empty
+            message.textContent = 'Please fill in all required fields.';
+            message.style.color = 'red';
+        } else if (!isValidEmail(email)) {
+            // Display error message if the email format is invalid
+            message.textContent = 'Please enter a valid email address.';
+            message.style.color = 'red';
+        } else {
+            // Clear any previous error messages
+            // message.textContent = '';
+
+            // Display success message
+            message.textContent = 'Form submitted successfully!';
+            message.style.color = 'green';
+        }
+    }
+
+    // Function to validate email format
+    function isValidEmail(email) {
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailPattern.test(email);
+    }
+
+    // Attach the validateForm function to the form submission event
+    document.getElementById('submitForm').addEventListener('submit', handleFormSubmit);
+})
